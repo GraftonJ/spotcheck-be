2eec63f110dd2374daf26c2afda0a9e0ca5d9ab1
Ed Phelps <edphelps1@gmail.com>
2018-12-12 11:26:08 -0700

    added seeds

--- seeds/001_users.js
+++ seeds/001_users.js
@@ -1,13 +1,16 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
+  return knex('users').del()
+    .then(() => {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+      return knex('users').insert([
+        {id: 1, fname: 'Cassi', lname: 'Bailey', email: 'cbaily@gmail.com', pswd_hash: '1234', dog_names: 'Spot the Dog'},
+        {id: 2, fname: 'Steve', lname: 'Wilson', email: 'swilson@gmail.com', pswd_hash: '1234', dog_names: 'Bender and Scooter'},
+        {id: 3, fname: 'Isabel', lname: 'Sampson', email: 'isampson@gmail.com', pswd_hash: '1234', dog_names: 'Luna'},
+      ])
+      .then(() => {
+				 // Moves id column (PK) auto-incremented to correct value after inserts
+				return knex.raw(`SELECT setval('users_id_seq', (SELECT MAX(id) FROM users))`)
+			})
+    })
+}
--- seeds/002_ratings.js
+++ seeds/002_ratings.js
@@ -1,13 +1,19 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
+  return knex('ratings').del()
+    .then(() => {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+      return knex('ratings').insert([
+        {id: 1, loca_id: '0A80IDh1Ag7jW4_D9sjQPQ', user_id: '1', rating: 4},
+        {id: 2, loca_id: '0A80IDh1Ag7jW4_D9sjQPQ', user_id: '3', rating: 3},
+        {id: 3, loca_id: 'MCd01xGZtmfSsH_RUj4pAA', user_id: '1', rating: 5},
+        {id: 4, loca_id: 'MCd01xGZtmfSsH_RUj4pAA', user_id: '2', rating: 1},
+        {id: 5, loca_id: 'MCd01xGZtmfSsH_RUj4pAA', user_id: '3', rating: 2},
+      ])
+      .then(() => {
+				 // Moves id column (PK) auto-incremented to correct value after inserts
+				return knex.raw(`SELECT setval('ratings_id_seq', (SELECT MAX(id) FROM ratings))`)
+			})
+    })
+}
