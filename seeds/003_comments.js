37e09f051eeeb845235daf82e41b06d188fa26fa
Elizabeth-Marie <elizabeth.brown843@gmail.com>
2018-12-12 11:35:02 -0700

    check_ins seeds done and changed comments seeds

--- seeds/003_comments.js
+++ seeds/003_comments.js
@@ -9,7 +9,7 @@ exports.seed = function(knex, Promise) {
         {id: 2, loca_id: 'MCd01xGZtmfSsH_RUj4pAA', user_id: 2, comment: "This is a great place to hang out on the patio and grab a quick lunch. They have water available if you need it for your dog"},
         {id: 3, loca_id: '0A80IDh1Ag7jW4_D9sjQPQ', user_id: 3, comment: "You can't bring your dog inside. But they have a nice patio area. The food is pretty good, too!"}
       ])
-      .then(function() { return knex.raw("SELECT setval('messages_id_seq', (SELECT MAX(id) FROM messages))")
+      .then(function() { return knex.raw("SELECT setval('comments_id_seq', (SELECT MAX(id) FROM comments))")
       })
     })
   }
--- seeds/004_check_ins.js
+++ seeds/004_check_ins.js
@@ -1,13 +1,16 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('check_ins').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
+      return knex('check_ins').insert([
+        {id: 1, user_id: 1, loca_id: 'FJo2jznp56MU_IdDcX038A'},
+        {id: 2, user_id: 2, loca_id: 'MCd01xGZtmfSsH_RUj4pAA'},
+        {id: 3, user_id: 3, loca_id: '0A80IDh1Ag7jW4_D9sjQPQ'}
+      ])
+      .then(() => {
+        return knex.raw(`SELECT setval('check_ins_id_seq', (SELECT MAX(id) FROM check_ins))`)
+      })
     });
 };
