82b727320711c4cff4b5e201afeac8a17ae8fe08
Josh <grafton.josh@gmail.com>
2018-12-12 12:10:11 -0700

    added comments route connection to app.js. Put line comments for other routes in app.js

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+//Add commentsRouter Below
+var commentsRouter = require('./routes/comments')
+//Add ratingsRouter Below
+//Add usersRouter Below
+//Add check_ins Router Below
 
 var app = express();
 
@@ -22,6 +26,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+//Add app.use comments Below
+app.use('/comments', commentsRouter)
+//Add app.use ratings below
+//Add app.use users below
+//Add app.use check_ins below
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
