6dc6248155662cefbf22b34af5298666495eb695
Ed Phelps <edphelps1@gmail.com>
2018-12-12 11:33:45 -0700

    fixed var char

--- migrations/20181212102837_comments.js
+++ migrations/20181212102837_comments.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('comments', function(table) {
     //TABLE COLUMN DEFINITON
     table.increments().notNullable().primary()
-    table.varChar('loca_id', 255).notNullable()
+    table.varchar('loca_id', 255).notNullable()
     table.text('comment').notNullable()
     table.integer('user_id').notNullable()
     table.foreign('user_id').references('users.id').onDelete('cascade')
