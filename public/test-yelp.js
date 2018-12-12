cd10073294eb004099ae923627fad82253a3428c
Ed Phelps <edphelps1@gmail.com>
2018-12-12 16:23:13 -0700

    testing the yelp route to get info for lsit of yelp loca_ids

--- public/test-yelp.js
+++ public/test-yelp.js
@@ -3,12 +3,14 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log("DOM loaded in test.js");
   console.log("before axios call");
-  axios.get('yelp/')
+  const param = { locaIds: ['MCd01xGZtmfSsH_RUj4pAA', '0A80IDh1Ag7jW4_D9sjQPQ']};
+  axios.post('yelp/', param)
     .then((res) => {
-      console.log("--- res: ", res);
       console.log("--- res.data: ", res.data);
+      const elem = document.getElementById('test');
+      elem.innerText = JSON.stringify(res.data);
     })
     .catch((error) => {
-      console.log("--- Error: ", error);;
+      console.log("--- Error: ", error);
     });
 });
--- routes/yelp.js
+++ routes/yelp.js
@@ -10,20 +10,49 @@ const { chkBodyParams } = require('./params'); // destructure the chkBodyParams 
 *  Get the comments, ratings, and checkins for array of restaurants
 *  @body loca_ids (array of YELP_LOCA_ID strings) [ 'DFWFRGEFD', 'FGRE65FG']
 *  Return
-*    200 { [
+*    200  {
+           [
 *           { locaId: YELP_LOCA_ID,
-              rating: 4.3,
               checkIns: 16,
               comments: [ { user: { id: 3, fname: "Sue", lname: "Grant"},
+                            rating: 4,
                             comment: "this is a comment"}
                         ] },
             { ... }
-         ] }
+           ]
+          }
 http GET localhost:3000/yelp
 ***************************************************** */
-router.get('/', (req, res, next) => {
-  console.log(`-- GET /yelp route`);
-  res.status(200).json({ mykey: 15 });
+// router.get('/', (req, res, next) => {
+router.post('/', (req, res, next) => {
+  console.log(`-- POST /yelp route`);
+  try {
+    // console.log("--- req: ", req);
+    const aLocaIds = req.body.locaIds;
+    // console.log('--- locaIds: ', aLocaIds);
+    // const sLocaIds = aLocaIds.reduce((str, locaId) => `${locaId} ${str}`, '');
+    // console.log('--- sLocaIds: ', sLocaIds);
+    let retVal = '';
+    knex('comments')
+      .whereIn('loca_id', aLocaIds)
+      .join('users', 'users.id', 'user_id')
+      .select('comments.*', 'users.fname', 'users.lname', 'users.dog_names')
+      .then((aRecs) => {
+        console.log("--> qry returning: ", aRecs);
+        retVal = aRecs;
+      })
+      .then(() => {
+        // console.log(">>> retVal: ", retVal);
+        res.status(200).json(retVal);
+      })
+      .catch((error) => {
+        next(routeCatch(`--- GET /yelp route`, error));
+      });
+  } catch (err) {
+    console.log("--- try/catch ERROR: ", err);
+    res.status(500).json(err);
+  }
+  // res.status(200).json({ mykey: 15 });
   // knex("users")
   //   .then((aRecs) => {
   //     res.status(200).json({ users: aRecs });
