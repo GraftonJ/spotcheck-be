fed3c9344dea4a167bb0911e6765d25c24d53df8
Ed Phelps <edphelps1@gmail.com>
2018-12-12 11:03:29 -0700

    added contents

--- migrations/20181212102803_users.js
+++ migrations/20181212102803_users.js
@@ -1,8 +1,14 @@
-
 exports.up = function(knex, Promise) {
-  
-};
-
+	return knex.schema.createTable(`users`, function(table) {
+		// TABLE COLUMN DEFINITIONS HERE
+		table.increments().notNullable().primary()
+		table.varchar(`fname`, 255).notNullable()
+		table.varchar(`lname`, 255).notNullable()
+		table.varchar(`email`, 255).notNullable()
+		table.varchar(`pswd_hash`, 255).notNullable()
+		table.varchar(`dog_names`, 255).notNullable()
+	})
+}
 exports.down = function(knex, Promise) {
-  
-};
+	return knex.schema.dropTableIfExists(`users`)
+}
--- migrations/20181212102846_ratings.js
+++ migrations/20181212102846_ratings.js
@@ -1,8 +1,13 @@
-
 exports.up = function(knex, Promise) {
-  
-};
-
+	return knex.schema.createTable(`ratings`, function(table) {
+		// TABLE COLUMN DEFINITIONS HERE
+		table.increments().notNullable().primary()
+		table.varchar(`loca_id`, 255).notNullable()
+		table.integer('user_id').notNullable()
+    table.foreign('user_id').references('users.id').onDelete('CASCADE')
+		table.integer('rating').notNullable()
+	})
+}
 exports.down = function(knex, Promise) {
-  
-};
+	return knex.schema.dropTableIfExists(`ratings`)
+}
