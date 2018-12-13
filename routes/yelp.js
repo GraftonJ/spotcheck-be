d24071dd97bce26a5c631f989378ae1ecb20ba92
Ed Phelps <edphelps1@gmail.com>
2018-12-12 20:56:28 -0700

    response working

--- routes/yelp.js
+++ routes/yelp.js
@@ -13,9 +13,10 @@ const { chkBodyParams } = require('./params'); // destructure the chkBodyParams 
 *  Return
 *    200  {
            [
-*           { locaId: YELP_LOCA_ID,
-              cntCheckIns: 16,
-              comments: [ { user: { id: 3, fname: "Sue", lname: "Grant"},
+            { locaId: 3232fT5656,
+              numCheckIns: 16,
+              comments: [ { locaId: 3232fT5656,
+                            user: { id: 3, fname: "Sue", lname: "Grant", dogName: "Sparky"},
                             rating: 4,
                             comment: "this is a comment"}
                         ] },
@@ -27,82 +28,74 @@ http GET localhost:3000/yelp
 // router.get('/', (req, res, next) => {
 router.post('/', (req, res, next) => {
   console.log(`-- POST /yelp route`);
-  try {
-    const aLocaIds = req.body.locaIds;
-    // console.log('--- locaIds: ', aLocaIds);
-    // const sLocaIds = aLocaIds.reduce((str, locaId) => `${locaId} ${str}`, '');
-    // console.log('--- sLocaIds: ', sLocaIds);
-    let aComments = [];
-    let aCheckIns = [];
-    // TODO: flatten and speed this up by doing Promise.all() for two queries
-    // TODO: make the query on check_ins table a SUM
 
-    // get comments for list of Yelp locaIds
-    console.log('1 ====== aLocaIds: ', aLocaIds);
-    knex('comments')
-      .whereIn('loca_id', aLocaIds)
-      .join('users', 'users.id', 'user_id')
-      .select('comments.*', 'users.fname', 'users.lname', 'users.dog_names')
-      .then((aRecs) => {
-        console.log("2 --> comments returning: ", aRecs);
-        aComments = aRecs;
+  const aLocaIds = req.body.locaIds;
+
+  let aComments = [];
+  let aCheckIns = [];
+
+  // TODO: flatten and speed this up by doing Promise.all() for two queries
+  // TODO: make the query on check_ins table a SUM
+
+  // get comments for list of Yelp locaIds
+  knex('comments')
+    .whereIn('loca_id', aLocaIds)
+    .join('users', 'users.id', 'user_id')
+    .select('comments.*', 'users.fname', 'users.lname', 'users.dog_names')
+    .then((aRecs) => {
+      // aComments = aRecs;
+      aComments = aRecs.map((rec) => {
+        return {
+          locaId: rec.loca_id,
+          user: {
+            id: rec.user_id,
+            fname: rec.fname,
+            lname: rec.lname,
+            dogNames: rec.dog_names,
+          },
+          rating: rec.rating,
+          comment: rec.comment,
+        };
       })
+    })
 
-      // get check_ins for list of Yelp locaIds
-      .then(() => {
-        console.log('2.5 then()');
-        knex('check_ins')
-          .whereIn('loca_id', aLocaIds)
-          .join('users', 'users.id', 'user_id')
-          .select('check_ins.*', 'users.fname', 'users.lname', 'users.dog_names')
-          .then((aRecs) => {
-            console.log("3 --> check_ins returning: ", aRecs);
-            aCheckIns = aRecs;
-          })
-          // catch errors from this level of then()s
-          .catch((error) => {
-            next(routeCatch(`--- INNER CATCH -- GET /yelp route`, error));
-          });
-      })
+    // get check_ins for list of Yelp locaIds
+    .then(() => {
+      knex('check_ins')
+        .whereIn('loca_id', aLocaIds)
+        .join('users', 'users.id', 'user_id')
+        .select('check_ins.*', 'users.fname', 'users.lname', 'users.dog_names')
+        .then((aRecs) => {
+          aCheckIns = aRecs;
+        })
 
-      // build and return data structure: array of Yelp loca_ids with summary
-      //   infromation on the comments and checkins for each location
-      .then(() => {
-        const aLocaInfo = [];
-        for (const locaId of aLocaIds) {
-          console.log('4 -----');
-          console.log("5 %% locaId, aCheckins", locaId, aLocaIds);
-          const objLocaInfo = {
-            locaId,
-            numCheckIns: aCheckIns.reduce((cnt, checkIn) => {
-              return cnt + ((checkIn.loca_id === locaId) ? 1 : 0);
-            }, 0),
-            comments: aComments.filter(comment => comment.loca_id === locaId),
-          };
+        // build and return data structure: array of Yelp loca_ids with summary
+        //   infromation on the comments and checkins for each location
+        .then(() => {
+          const aLocaInfo = [];
+          for (const locaId of aLocaIds) {
 
-          aLocaInfo.push(objLocaInfo);
-        }
+            const objLocaInfo = {
+              locaId,
+              numCheckIns: aCheckIns.reduce((cnt, checkIn) => {
+                return cnt + ((checkIn.loca_id === locaId) ? 1 : 0);
+              }, 0),
+              comments: aComments.filter(comment => comment.locaId === locaId),
+            };
 
-
-        res.status(200).json(aLocaInfo);
-      })
-
-      // catch errors
-      .catch((error) => {
-        next(routeCatch(`--- GET /yelp route`, error));
-      });
-  } catch (err) {
-    console.log("--- try/catch ERROR: ", err);
-    res.status(500).json(err);
-  }
-  // res.status(200).json({ mykey: 15 });
-  // knex("users")
-  //   .then((aRecs) => {
-  //     res.status(200).json({ users: aRecs });
-  //   })
-  //   .catch((error) => {
-  //     next(routeCatch(`--- GET /users route`, error));
-  //   });
+            aLocaInfo.push(objLocaInfo);
+          }
+          res.status(200).json(aLocaInfo);
+        })
+        // catch errors
+        .catch((error) => {
+          next(routeCatch(`--- GET /yelp route`, error));
+        });
+    })
+    .catch((err) => {
+      console.log("--- try/catch ERROR: ", err);
+      res.status(500).json(err);
+    });
 });
 
 
