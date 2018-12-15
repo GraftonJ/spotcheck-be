60c3ecbd87603ac1d0d2f987c5e45426bfb1b884
Ed Phelps <edphelps1@gmail.com>
2018-12-15 15:37:52 -0700

    added user to seeds

--- seeds/001_users.js
+++ seeds/001_users.js
@@ -7,6 +7,7 @@ exports.seed = function(knex, Promise) {
         {id: 1, name: 'Cassi Bailey', email: 'cbaily@gmail.com', pswd_hash: '1234', dog_names: 'Spot the Dog'},
         {id: 2, name: 'Steve Wilson', email: 'swilson@gmail.com', pswd_hash: '1234', dog_names: 'Bender and Scooter'},
         {id: 3, name: 'Isabel Sampson', email: 'isampson@gmail.com', pswd_hash: '1234', dog_names: 'Luna'},
+        {id: 4, name: 'New User', email: 'nuser@gmail.com', pswd_hash: 'secret', dog_names: 'Luna'},
       ])
       .then(() => {
 				 // Moves id column (PK) auto-incremented to correct value after inserts
