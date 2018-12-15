f3bd235c45d27df96dd216dc26e8f524c8e24121
Ed Phelps <edphelps1@gmail.com>
2018-12-15 15:50:31 -0700

    updated seeds to use real tokens

--- seeds/001_users.js
+++ seeds/001_users.js
@@ -4,10 +4,10 @@ exports.seed = function(knex, Promise) {
     .then(() => {
       // Inserts seed entries
       return knex('users').insert([
-        {id: 1, name: 'Cassi Bailey', email: 'cbaily@gmail.com', pswd_hash: '1234', dog_names: 'Spot the Dog'},
-        {id: 2, name: 'Steve Wilson', email: 'swilson@gmail.com', pswd_hash: '1234', dog_names: 'Bender and Scooter'},
-        {id: 3, name: 'Isabel Sampson', email: 'isampson@gmail.com', pswd_hash: '1234', dog_names: 'Luna'},
-        {id: 4, name: 'New User', email: 'nuser@gmail.com', pswd_hash: 'secret', dog_names: 'Luna'},
+        {id: 1, name: 'Cassi Bailey', email: 'cbaily@gmail.com', pswd_hash: '$2b$04$ETZS5t9G1eG981XrEC1duu0OjfeME3yzJT1iTFsBk51t69dFfJNNe', dog_names: 'Spot the Dog'},
+        {id: 2, name: 'Steve Wilson', email: 'swilson@gmail.com', pswd_hash: '$2b$04$ETZS5t9G1eG981XrEC1duu0OjfeME3yzJT1iTFsBk51t69dFfJNNe', dog_names: 'Bender and Scooter'},
+        {id: 3, name: 'Isabel Sampson', email: 'isampson@gmail.com', pswd_hash: '$2b$04$ETZS5t9G1eG981XrEC1duu0OjfeME3yzJT1iTFsBk51t69dFfJNNe', dog_names: 'Luna'},
+        {id: 4, name: 'New User', email: 'nuser@gmail.com', pswd_hash: '$2b$04$ETZS5t9G1eG981XrEC1duu0OjfeME3yzJT1iTFsBk51t69dFfJNNe', dog_names: 'Luna'},
       ])
       .then(() => {
 				 // Moves id column (PK) auto-incremented to correct value after inserts
