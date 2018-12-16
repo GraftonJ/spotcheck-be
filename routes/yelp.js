e1d3e3c28572ffedb671325b4ef279ca6c355bc5
Ed Phelps <edphelps1@gmail.com>
2018-12-16 14:01:35 -0700

    db changed from fname/lname to name, chacgd return "locaId" to "id"

--- routes/yelp.js
+++ routes/yelp.js
@@ -6,30 +6,32 @@ const routeCatch = require('./routeCatch');
 const { chkBodyParams } = require('./params'); // destructure the chkBodyParams out of require('./params') returned object
 
 /* **************************************************
-*  GET /
+*  POST /
 *  Get the comments, ratings, and checkins for array of restaurants
-*  @body loca_ids (array of YELP_LOCA_ID strings)
-*                   [ 'MCd01xGZtmfSsH_RUj4pAA', '0A80IDh1Ag7jW4_D9sjQPQ']
-*  Return
+*  @body locaIds (array of YELP_LOCA_ID strings)
+*                   ['MCd01xGZtmfSsH_RUj4pAA', '0A80IDh1Ag7jW4_D9sjQPQ']
+*  Return an array with ALL of the yelp ids (fills in 0 and [] if there
+*               weren't any checkins or comments for the location)
 *    200  {
            [
-            { locaId: 3232fT5656,
+            { id: 3232fT5656,
               numCheckIns: 16,
               comments: [ { locaId: 3232fT5656,
-                            user: { id: 3, fname: "Sue", lname: "Grant", dogName: "Sparky"},
+                            user: { id: 3, name: "Sue Grant", dogName: "Sparky"},
                             rating: 4,
                             comment: "this is a comment"}
                         ] },
             { ... }
            ]
           }
-http GET localhost:3000/yelp
+HTTP tested with /public/test.html which uses /public/test-yelp.js
 ***************************************************** */
 // router.get('/', (req, res, next) => {
 router.post('/', (req, res, next) => {
   console.log(`-- POST /yelp route`);
 
   const aLocaIds = req.body.locaIds;
+  console.log('aLocaIds = ', aLocaIds);
 
   let aComments = [];
   let aCheckIns = [];
@@ -41,7 +43,7 @@ router.post('/', (req, res, next) => {
   knex('comments')
     .whereIn('loca_id', aLocaIds)
     .join('users', 'users.id', 'user_id')
-    .select('comments.*', 'users.fname', 'users.lname', 'users.dog_names')
+    .select('comments.*', 'users.name', 'users.dog_names')
     .then((aRecs) => {
       // aComments = aRecs;
       aComments = aRecs.map((rec) => {
@@ -49,8 +51,7 @@ router.post('/', (req, res, next) => {
           locaId: rec.loca_id,
           user: {
             id: rec.user_id,
-            fname: rec.fname,
-            lname: rec.lname,
+            name: rec.name,
             dogNames: rec.dog_names,
           },
           rating: rec.rating,
@@ -64,19 +65,20 @@ router.post('/', (req, res, next) => {
       knex('check_ins')
         .whereIn('loca_id', aLocaIds)
         .join('users', 'users.id', 'user_id')
-        .select('check_ins.*', 'users.fname', 'users.lname', 'users.dog_names')
+        .select('check_ins.*', 'users.name', 'users.dog_names')
         .then((aRecs) => {
           aCheckIns = aRecs;
         })
 
         // build and return data structure: array of Yelp loca_ids with summary
-        //   infromation on the comments and checkins for each location
+        //   infromation on the comments and checkins for EACH location.
+        //   Will fill in 0 and [] for location without checkins or comments.
         .then(() => {
           const aLocaInfo = [];
           for (const locaId of aLocaIds) {
 
             const objLocaInfo = {
-              locaId,
+              id: locaId,
               numCheckIns: aCheckIns.reduce((cnt, checkIn) => {
                 return cnt + ((checkIn.loca_id === locaId) ? 1 : 0);
               }, 0),
@@ -97,6 +99,4 @@ router.post('/', (req, res, next) => {
       res.status(500).json(err);
     });
 });
-
-
 module.exports = router;
